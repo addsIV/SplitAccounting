bd1dc3eb5cca7c0b82815015c24f4dc71d0f43b7
Kevin Lin using laptop <kl13245768@gmail.com>
2021-09-23 13:51:18 +0800

    加入專案檔案。

---
+++ SplitAccounting/MVVM/ViewModel/MainViewModel.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SplitAccounting.Core;
+
+namespace SplitAccounting.MVVM.ViewModel
+{
+    internal class MainViewModel : ObsevableObject
+    {
+        public RelayCommand HomeViewCommand { get; set; }
+        public RelayCommand DiscoveryViewCommand { get; set; }
+        public HomeViewModel HomeVM { get; set; }
+        public DiscoveryViewModel DiscoveryVM { get; set; }
+
+        private object _currentView;
+
+        public object CurrentView
+        {
+            get { return _currentView; }
+            set
+            {
+                _currentView = value;
+                onPorpertyChanged();
+            }
+        }
+
+        public MainViewModel()
+        {
+            HomeVM = new HomeViewModel();
+            DiscoveryVM = new DiscoveryViewModel();
+
+            CurrentView = HomeVM;
+
+            HomeViewCommand = new RelayCommand(p =>
+            {
+                CurrentView = HomeVM;
+            });
+
+            DiscoveryViewCommand = new RelayCommand(p =>
+            {
+                CurrentView = DiscoveryVM;
+            });
+        }
+    }
+}
